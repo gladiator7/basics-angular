f8a3c6f49198a2e547249eb809dc80f5ca9adaa7
gladiator7 <prabhdevgun01@gmail.com>
2019-04-14 23:12:45 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,30 +4,30 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { AccountComponent } from './account/account.component';
-import { NewAccountComponent } from './new-account/new-account.component';
+import { HomeComponent } from './home/home.component';
+import { UsersComponent } from './users/users.component';
+import { ServersComponent } from './servers/servers.component';
+import { UserComponent } from './users/user/user.component';
+import { EditServerComponent } from './servers/edit-server/edit-server.component';
+import { ServerComponent } from './servers/server/server.component';
+import { ServersService } from './servers/servers.service';
 
-import { LoggingService } from './logging.service';
-import { AccountService } from './account.service';
-
-
-// there is hierarchial service instances.ex parent and its child has same instance of service but if we add new instance to provider in a component then new instance is add so lead to new instance and values will be different
 @NgModule({
   declarations: [
     AppComponent,
-    AccountComponent,
-    NewAccountComponent
+    HomeComponent,
+    UsersComponent,
+    ServersComponent,
+    UserComponent,
+    EditServerComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [
-    AccountService,
-    LoggingService
-
-  ],
+  providers: [ServersService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/logging.service.ts
+++
@@ -1,9 +0,0 @@
-export class LoggingService{
-logStatusChange(status : string ){
-  console.log('A server status changed, new status: ' + status);
-
-}
-}
-
-//helps to follow DRY code
-//use this service to add code which is used in multiple components

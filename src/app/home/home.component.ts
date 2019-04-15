40669ee108a9274c90c1d808738f5d892d22b1ca
gladiator7 <prabhdevgun01@gmail.com>
2019-04-15 14:19:35 +0530

    8 files modified

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+onLoadServers(){
+  this.router.navigate(['/servers']);
+
+  // if path is "/servers" then it is absolute path
+  // if "servers" it is relative path
+}
 }
